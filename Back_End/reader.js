6a882f3485a0aa5382a11f7c334335f2559fc798
GFury <gsamardjiev@gmail.com>
2014-11-05 17:26:09 +0200

    Second Major Modification - Mongo Integration Done.

--- Back_End/reader.js
+++ Back_End/reader.js
@@ -1,57 +1,54 @@
 var feed = require("feed-read");
 var http = require("http");
-var mongoose = require("mongoose")
+var mongoose = require("mongoose");
 
 
+
+// Here it goes
 feed("http://feeds.feedburner.com/RockPaperShotgun?format=xml", function(err, articles){
     if (err) throw err;
 //    console.log(articles);
 
-    var feedTitles=[];
-    var feedContent=[];
+    var feedTitles =[];
+    var feedContent = [];
     for (var i=0; i<articles.length; i++){
       feedTitles[i] = articles[i].title;
       feedContent[i] = articles[i].content;
     };
 //    console.log(feedTitles);
 //    console.log(feedContent);
-
-    var db = mongoose.createConnection("mongodb://192.168.1.883/test");
-    db.on("error",console.error);
+    // The real way to connect to a mongo! Bongo Bongo!
+    mongoose.connect("mongodb://localhost/test3")
+    var db = mongoose.connection;
+   // var db = mongoose.createConnection("mongodb://localhost/test");
+    db.on("error", console.error);
     db.once("open",function(){
-//  Mongo DB schema
-      var feedSchema = new mongoose.Schema({
-        feed_title:{type:string},
-        feed_content: {type:content}
-      });
-
-      var firstFeed = {
-        feed_title: feedTitles[0],
-        feed_content: feedContent[0] 
-      };
-      var first_push = new feedSchema(firstFeed);
-      // Saving the feed
-      first_push.save(function(error, data){
-        if(error){
-          console.log(error);
-        }
-        else{
-          console.log(data);
-        }
-      });
-      
+      console.log("DB Works");
     });
-
+    //*Mongo Schema Generation
+    var feedSchema = new mongoose.Schema({
+      title: String,
+      content: String
+      });
+    //*Mongo Model
+    for (var i=0; i<articles.length; i++){
+      var feedPusher_model = mongoose.model("feedPusher", feedSchema);
+      var feed_push = new feedPusher_model;
+      feed_push.title = articles[i].title;
+      feed_push.content = articles[i].content;
+      //*Mongo save
+      feed_push.save(function(err,data){
+        if(err)
+        {
+          console.log(err);
+        }
+        else
+        {
+          console.log(data);
+        //  mongoose.disconnect();
+        }
+      });
+    };
 
 });
 
-
-
-// console.log(feeds)
-
-
-/*http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Only Testing\n');
-}).listen(3000, '0.0.0.0');
-*/
