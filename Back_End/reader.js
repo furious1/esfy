d6e6aa1adaadf00b62ee160d6377db3ab6dfbf29
GFury <gsamardjiev@gmail.com>
2014-11-15 00:57:20 +0200

    [BE.0.0.2] Simple Scheduler and DB Connection implemented.

--- Back_End/reader.js
+++ Back_End/reader.js
@@ -1,54 +1,55 @@
 var feed = require("feed-read");
-var http = require("http");
 var mongoose = require("mongoose");
 
 
-
-// Here it goes
-feed("http://feeds.feedburner.com/RockPaperShotgun?format=xml", function(err, articles){
-    if (err) throw err;
-//    console.log(articles);
-
-    var feedTitles =[];
-    var feedContent = [];
-    for (var i=0; i<articles.length; i++){
-      feedTitles[i] = articles[i].title;
-      feedContent[i] = articles[i].content;
-    };
-//    console.log(feedTitles);
-//    console.log(feedContent);
-    // The real way to connect to a mongo! Bongo Bongo!
-    mongoose.connect("mongodb://localhost/test3")
-    var db = mongoose.connection;
-   // var db = mongoose.createConnection("mongodb://localhost/test");
-    db.on("error", console.error);
-    db.once("open",function(){
-      console.log("DB Works");
-    });
-    //*Mongo Schema Generation
-    var feedSchema = new mongoose.Schema({
-      title: String,
-      content: String
-      });
-    //*Mongo Model
-    for (var i=0; i<articles.length; i++){
-      var feedPusher_model = mongoose.model("feedPusher", feedSchema);
-      var feed_push = new feedPusher_model;
-      feed_push.title = articles[i].title;
-      feed_push.content = articles[i].content;
-      //*Mongo save
-      feed_push.save(function(err,data){
-        if(err)
-        {
-          console.log(err);
-        }
-        else
-        {
-          console.log(data);
-        //  mongoose.disconnect();
-        }
-      });
-    };
-
+//MongoDB via mongoose connection
+mongoose.connect("mongodb://localhost/test15");
+var db = mongoose.connection;
+db.on("error", console.error);
+db.once("open", function(){
+    console.log("DB Connection Test Succesfull")
 });
+//MongoDB Schema
+var feedSchema = new mongoose.Schema({
+      title: String,
+      //content: String
+      num: Number
+});
+var feedPusher_model = mongoose.model("feedPusher", feedSchema);
+// Declaring global loops
+var feed_push = [];
+var feed_validate = [];
+
+//Feed Reader
+function feeder(){
+    feed("http://feeds.feedburner.com/RockPaperShotgun?format=xml", function(err, articles){
+            if (err) throw err;
+            for (var i=0; i<articles.length; i++){
+            feed_push[i] = new feedPusher_model();
+            feed_push[i].num = i+1;
+            feed_push[i].title = articles[i].title;
+            feed_push[i].content = articles[i].content;
+            //Feed Validation + Save
+            feed_validate[i] = feedPusher_model.find({title:articles[i].title});
+            feed_validate[i].exec(function (err,dbresponse){
+                if (err){console.log(err);}
+                if (dbresponse.title == feed_push.title){
+                    console.log("Article is duplicate");
+                }
+                else{
+                    feed_push[i].save(function(err,data){
+                        if(err){console.log(err);}
+                        else{
+                            console.log(data);
+                            }
+                    });
+
+                }
+            });
+        }
+    });
+}
+var timeInterval = 2000;
+
+setInterval(feeder, timeInterval)
 
